10ec9c4a5fcc0f3153bbe99b81ae71ed33216639
Tom Janssen <tom@dcc.team>
2025-07-08 22:41:32 +0200

    visibility toggleable issue

---
+++ app/Filament/Resources/Users/Pages/ListUsers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\Users\Pages;
+
+use App\Filament\Resources\Users\UserResource;
+use Filament\Actions\Action;
+use Filament\Actions\CreateAction;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Resources\Pages\ListRecords;
+
+class ListUsers extends ListRecords
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
+}
